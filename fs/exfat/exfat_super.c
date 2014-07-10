fa3c76a634ad4aa00c32546567ede00e7ce9e9aa
EvolutionMod <kernelcraft@gmail.com>
2014-07-10 10:15:23 -0400

    exFAT: fix merge derp
    Signed-off-by: EvolutionMod <kernelcraft@gmail.com>

--- fs/exfat/exfat_super.c
+++ fs/exfat/exfat_super.c
@@ -1792,11 +1792,9 @@ static const match_table_t exfat_tokens = {
 	{Opt_err_cont, "errors=continue"},
 	{Opt_err_panic, "errors=panic"},
 	{Opt_err_ro, "errors=remount-ro"},
-#if EXFAT_CONFIG_DISCARD
-	{Opt_htc_hack, "utf8"},
 #ifdef CONFIG_EXFAT_DISCARD
 	{Opt_discard, "discard"},
-#endif
+#endif /* CONFIG_EXFAT_DISCARD */
 	{Opt_err, NULL}
 };
 
@@ -1890,13 +1888,10 @@ static int parse_options(char *options, int silent, int *debug,
 		case Opt_discard:
 			opts->discard = 1;
 			break;
-<<<<<<< HEAD
-#endif
-=======
+
 #endif /* CONFIG_EXFAT_DISCARD */
 		case Opt_htc_hack:
 			break;
->>>>>>> d32ed6e... exfat: more HTC vold mount option hacks!
 		default:
 			if (!silent) {
 				printk(KERN_ERR "[EXFAT] Unrecognized mount option %s or missing value\n", p);
