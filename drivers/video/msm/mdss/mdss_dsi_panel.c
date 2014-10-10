cc10f385545242b49b5eebf6332b8e2e3c1e005b
buckmarble <forrestmarble@gmail.com>
2014-10-10 12:40:07 -0400

    sync backlight customization from flar2

--- drivers/video/msm/mdss/mdss_dsi_panel.c
+++ drivers/video/msm/mdss/mdss_dsi_panel.c
@@ -26,7 +26,10 @@
 #include <mach/debug_display.h>
 #include <linux/msm_mdp.h>
 
-#include <linux/moduleparam.h>
+
+
+#include <linux/kobject.h>
+#include <linux/sysfs.h>
 
 #define DT_CMD_HDR 6
 #define WLED_MAX_LEVEL	4095
@@ -34,8 +37,51 @@
 DEFINE_LED_TRIGGER(bl_led_trigger);
 DEFINE_LED_TRIGGER(bl_led_i2c_trigger);
 
-bool backlight_dimmer = false;
-module_param(backlight_dimmer, bool, 0755);
+
+/* Backlight dimmer */
+static int backlight_dimmer = 0;
+
+static ssize_t backlight_dimmer_show(struct kobject *kobj,
+		struct kobj_attribute *attr, char *buf)
+{
+	ssize_t count = 0;
+	count += sprintf(buf, "%d\n", backlight_dimmer);
+	return count;
+}
+
+static ssize_t backlight_dimmer_store(struct kobject *kobj,
+		struct kobj_attribute *attr, const char *buf, size_t count)
+{
+
+	sscanf(buf, "%d ",&backlight_dimmer);
+	if (backlight_dimmer < 0 || backlight_dimmer > 3) {
+		backlight_dimmer = 0;
+	}
+	return count;
+}
+
+static struct kobj_attribute backlight_dimmer_attribute = 
+	__ATTR(backlight_dimmer, 0666,
+		backlight_dimmer_show,
+		backlight_dimmer_store);
+
+static struct attribute *backlight_dimmer_attrs[] =
+	{
+		&backlight_dimmer_attribute.attr,
+		NULL,
+	};
+
+static struct attribute_group backlight_dimmer_attr_group =
+	{
+		.attrs = backlight_dimmer_attrs,
+	};
+
+
+static struct kobject *backlight_dimmer_kobj;
+
+/* end Backlight Dimmer */
+
+
 
 void mdss_dsi_panel_pwm_cfg(struct mdss_dsi_ctrl_pdata *ctrl)
 {
@@ -226,9 +272,13 @@ static void mdss_dsi_panel_bklt_dcs(struct mdss_dsi_ctrl_pdata *ctrl, int level)
 	pr_debug("%s: level=%d\n", __func__, level);
 
 	if (!pinfo->act_brt) {
-		if (backlight_dimmer) {
-			led_pwm1[1] = (unsigned char)shrink_pwm(level, 1, 25, 125);
-		} else {
+		//backlight dimmer	
+		if (backlight_dimmer == 1) {  //original is 6,56,255 my first version was 1,25,125
+			led_pwm1[1] = (unsigned char)shrink_pwm(level, 1, 45, 175);
+		} else if (backlight_dimmer == 2) {
+			led_pwm1[1] = (unsigned char)shrink_pwm(level, 1, 35, 130);
+		//stock
+		} else	{
 			led_pwm1[1] = (unsigned char)shrink_pwm(level, ctrl->pwm_min, ctrl->pwm_default, ctrl->pwm_max);
 		}
 	} else {
@@ -1191,6 +1241,19 @@ int mdss_dsi_panel_init(struct device_node *node,
 	ctrl_pdata->off = mdss_dsi_panel_off;
 	ctrl_pdata->panel_data.set_backlight = mdss_dsi_panel_bl_ctrl;
 	ctrl_pdata->panel_data.display_on = mdss_dsi_display_on;
+
+/* Backlight Dimmer */
+	backlight_dimmer_kobj = kobject_create_and_add("backlight_dimmer", NULL);
+	if (backlight_dimmer_kobj == NULL) {
+		pr_warn("%s kobject create failed!\n", __func__);
+        }
+
+	rc = sysfs_create_group(backlight_dimmer_kobj, &backlight_dimmer_attr_group);
+        if (rc) {
+		pr_warn("%s sysfs file create failed!\n", __func__);
+	}
+
+/* end Backlight Dimmer */
 
 	return 0;
 }
