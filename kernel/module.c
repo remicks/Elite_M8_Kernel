77170faba2bf1edfd43c91e106f5d571f49af664
buckmarble <forrestmarble@gmail.com>
2014-11-12 18:59:05 -0500

    allow stock modules to load take 2

--- kernel/module.c
+++ kernel/module.c
@@ -1110,9 +1110,14 @@ static int check_version(Elf_Shdr *sechdrs,
 {
 	unsigned int i, num_versions;
 	struct modversion_info *versions;
-	
-        /*Use all stock modules, if need be.*/
-	if(!strncmp("ko", mod->name, 4))
+
+	if(!strncmp("wlan", mod->name, 4))
+		return 1;
+
+	if(!strncmp("moc_", mod->name, 4))
+		return 1;
+
+	if(!strncmp("texfat", mod->name, 6))
 		return 1;
 
 	/* Exporting module didn't supply crcs?  OK, we're already tainted. */
