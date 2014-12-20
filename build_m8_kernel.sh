856d3132edcb5f64530edc05da39363b9fbe3e2c
buckmarble <forrestmarble@gmail.com>
2014-12-20 10:04:23 -0500

    build elite defconfig

--- build_m8_kernel.sh
+++ build_m8_kernel.sh
@@ -39,7 +39,7 @@ find ./ -name '*~' | xargs rm
 # rm compile.log
 
 # make kernel
-make 'm8_defconfig'
+make 'elite_defconfig'
 make -j`grep 'processor' /proc/cpuinfo | wc -l` CROSS_COMPILE=$TOOLCHAIN #>> compile.log 2>&1 || exit -1
 
 # Grab modules & zImage
